62c5fdf68721397f3984839f9eb21d34c9b93307
Christina Bilbo <cbilbo@Christinas-MacBook-Pro.local>
2015-12-12 14:53:12 -0700

    Revert "finished Message class"
    This reverts commit 663fcf3860764c073423c9336f5a188a14c25249.

--- app/src/main/java/com/tonikamitv/loginregister/Message.java
+++ app/src/main/java/com/tonikamitv/loginregister/Message.java
@@ -1,21 +1,54 @@
 package com.tonikamitv.loginregister;
 
-import java.io.Serializable;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
-public class Message implements Serializable {
+/**
+ * Created by cbilbo on 12/12/15.
+ */
+public class Message extends Activity {
 
-    String name, username, password, message;
-    int age;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-    public Message(String name, int age, String username, String password, String message) {
-        this.name = name;
-        this.age = age;
-        this.username = username;
-        this.password = password;
-        this.message = message;
+        getLayoutInflater().setFactory(new LayoutInflater.Factory() {
+
+            @Override
+            public View onCreateView(String name, Context context, AttributeSet attrs) {
+                View v = tryInflate(name, context, attrs);
+                if (v instanceof TextView) {
+                    setTypeFace((TextView) v);
+                }
+                return v;
+            }
+        });
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
     }
 
-    public Message(String username, String message) {
-        this("", -1, username, "", message);
+    private View tryInflate(String name, Context context, AttributeSet attrs) {
+        LayoutInflater li = LayoutInflater.from(context);
+        View v = null;
+        try {
+            v = li.createView(name, null, attrs);
+        } catch (Exception e) {
+            try {
+                v = li.createView("android.widget." + name, null, attrs);
+            } catch (Exception e1) {
+            }
+        }
+        return v;
+    }
+
+    private void setTypeFace(TextView tv) {
+        tv.setTypeface(Typeface.createFromAsset(getAssets(), "AManoRegulold.ttf"));
     }
 }
--- app/src/main/java/com/tonikamitv/loginregister/Register.java
+++ app/src/main/java/com/tonikamitv/loginregister/Register.java
@@ -5,14 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+//import android.support.v7.app.ActionBarActivity;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-//import android.support.v7.app.ActionBarActivity;
 
 
 public class Register extends Activity implements View.OnClickListener{
